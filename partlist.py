78e6bbc643a7861c808ee7bbc50a6b74b5bc7b61
Esa Prakasa <esa.prakasa@gmail.com>
2020-03-11 07:45:21 +0700

    Add files via upload

---
+++ partlist.py
@@ -0,0 +1,13 @@
+nums = [10, 20, 30, 40, 50]
+print('Tampilkan semua bilangan')
+print(nums)
+print('Tampilkan dari indeks 1 dan <3')
+print(nums[1:3])
+print('Tampilkan dari indeks awal dan <2')
+print(nums[:2])
+print('Tampilkan semua indeks selisih 2')
+print(nums[: :2])
+print('Tampilkan 2 indeks terakhir')
+print(nums[-2:])
+
+

0267129d7a1d6114122686d359f1445d43744535
Esa Prakasa <esa.prakasa@gmail.com>
2023-06-14 05:08:37 +0700

    Add files via upload

---
+++ soal5_uas.py
@@ -0,0 +1,19 @@
+import os
+import matplotlib.pyplot as plt
+os.system("cls")
+
+x =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y1 = [10, 20, 30, 20, 20, 40, 50, 60, 70, 60]
+y2 = [50, 40, 30, 40, 50, 60, 70, 80, 90, 80]
+y3 = [100, 90, 100, 90, 100, 90, 100, 90, 100, 90]
+
+plt.###(###,###,color=###, linewidth = 6)
+plt.###(###,###,color=###, linewidth = 3)
+plt.###(###,###,color=###, linewidth = 2)
+
+plt.xlabel(###)
+plt.ylabel(###)
+plt.grid(True)
+
+plt.show()
+

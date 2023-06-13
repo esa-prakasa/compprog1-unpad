403ae9a054df1cdacdc339754382c3f430f61378
Esa Prakasa <esa.prakasa@gmail.com>
2023-06-13 21:47:51 +0700

    Add files via upload

---
+++ soal1_uas.py
@@ -0,0 +1,22 @@
+import os
+os.system("cls")
+
+def penjumlahan(x1, x2):
+    ####
+    return ####
+
+def pengurangan(x1, x2):
+    ####
+    return ####
+
+"####" = int(input("Berikan bilangan ke-1: "))
+"####" = int(input("Berikan bilangan ke-2: "))
+
+hasilJumlah = pen"###"(bil1, bil2)
+hasilKurang = peng"###"(bil1, bil2)
+
+totalPerkalian = hasil### * hasil####
+
+print("Hasil jumlah dua bilangan adalah : "+str(###))
+print("Hasil kurang dua bilangan adalah : "+str(###))
+print("Hasil Total Perkalian kedua hasil tersebut : "+str(###))

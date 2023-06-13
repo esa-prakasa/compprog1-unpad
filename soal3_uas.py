1efd57fe81b90cde3fb661e4128b3d14d4157e12
Esa Prakasa <esa.prakasa@gmail.com>
2023-06-14 03:58:45 +0700

    Add files via upload

---
+++ soal3_uas.py
@@ -0,0 +1,21 @@
+import os
+os.system("cls")
+
+class Item:
+    def __init__(self, nama, harga, jumlah, warna):
+        self.### = ###
+        self.### = ###
+        self.### = ###
+        self.### = ###
+    
+br1 = Item(###, 300000, ###, ###)
+br2 = Item(###, 150000, ###, ###)
+br3 = Item(###, 450000, ###, ###)
+
+print("Nama-nama Barang: \n"+###+", "+###+", "+###)
+print("\nNama Barang 1 dan Jumlah: "+###+"--"+###)
+print("Nama Barang 2 dan Jumlah: "+###+"--"+###)
+print("Nama Barang 3 dan Jumlah: "+###+"--"+###)
+
+hargaBarangTotal = ###*### + ###*### + ###*### 
+print("\nHarga Total semua barang adalah: "+'{:,}'.format(hargaBarangTotal))
